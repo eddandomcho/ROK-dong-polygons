f70e6dee5a27abeb878c4f9ab4a3bd821216a6b9
조민웅's Macbook <minwoongcho@jomin-ungsui-MacBookAir.local>
2026-06-05 18:03:55 +0900

    geojson json 다시 만들기, csv_transform 문제 해결

---
+++ indiv_script.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+import json
+import models.csv_transform as ct
+import models.geojson_polygon as gp
+import models.polygon as p
+
+df = pd.read_csv("legal_dong_code_mstr.csv")
+df = df[df.status == "존재"]
+df = df[df.level == 3]
+
+sido_code_list = df["code"].astype(str).str[0:2].astype(int).unique().tolist()
+print("Generated sido_code_list!")
+
+i = 52
+code_list_sido = ct.code_list_sido(i)
+print(code_list_sido)
+print(f"Generated code_list_sido for sido code {i}!")
+
+p.write_polygon_json_list(str(i), code_list_sido)
+print(f"Wrote polygon json files for sido code {i}!")
+
+gp.transform_to_geojson_list(f"json_files/{i}", f"geojson_files/{i}")
+print(f"Wrote polygon GeoJSON files for sido code {i}!")
