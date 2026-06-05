42eb2727bbe19b0ab4a6dc18c41c3c65fbddd8a5
조민웅's Macbook <minwoongcho@jomin-ungsui-MacBookAir.local>
2026-06-05 17:37:18 +0900

    붚필요한 파일 삭제

--- codeTrials.py
+++
@@ -1,3 +0,0 @@
-import models.csv_transform as ct
-
-print(ct.code_list_sigungu(11, 560))
--- pandas_analysis.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("legal_dong_code_mstr.csv")
-
-print(len(df[df.level == 3]))
